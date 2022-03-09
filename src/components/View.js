27957f0e71e970ee3a83581e63fc8ca54acb807f
krishxD <krishdudi001@gmail.com>
2022-03-09 12:25:42 +0530

    New database link has been added

---
+++ src/components/View.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const View = () => {
+    
+
+  return (
+    <>
+        <div className="contanier">
+            <h1></h1>    
+        </div>
+    </>
+  )
+}
+
+export default View
